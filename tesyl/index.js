887e4908c9460020e3f8f2435ffeddcd5de3f2ec
Uoc Azizah <uocdev.github.io@gmail.com>
2026-01-20 16:59:13 +0800

    index.js: update index.js for testing new cout

--- tesyl/index.js
+++ tesyl/index.js
@@ -1,5 +1,7 @@
-import { cout } from '@uocdev/yauiselib';
+import { cout, coutint } from '@uocdev/yauiselib';
 
 cout('hello :3');
 cout("halooo juga");
 cout("aku Azizah :3");
+coutint(28);
+coutint(2.81);
