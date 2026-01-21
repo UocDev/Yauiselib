93190e7a3916677c5c079d8d9525b2970256c764
Uoc Azizah <uocdev.github.io@gmail.com>
2026-01-21 15:15:32 +0800

    cout.ts: add error message

--- src/cout/cout.ts
+++ src/cout/cout.ts
@@ -1,4 +1,8 @@
-export function cout(text: string): void {
-  console.log(`${text}`);
+export function cout(...$text: string[]): void {
+  if (!$text.every($text => typeof $text === "string")) {
+    throw new TypeError("cout only accept string");
+  }
+  console.log(...$text);
 }
+
 
