2e290088fe1d1bc3c030b6856dedeb5127e91d46
Uoc Azizah <uocdev.github.io@gmail.com>
2026-01-21 15:15:56 +0800

    coutint.ts: add error message

--- src/cout/coutint.ts
+++ src/cout/coutint.ts
@@ -1,3 +1,10 @@
-export function coutint(int: number): void {
-   console.log(`${int}`);
+export function coutint(...$int: number[]): void {
+  if (!$int.every(v => typeof v === "number")) {
+    throw new TypeError(
+      "coutint only accept number.\n" +
+      "looking for string?\n" +
+      "cout();"
+    );
+  }
+  console.log(...$int);
 }
